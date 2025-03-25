4d6ea8f0661ea8f26296e1ad7da8ff1bd9666f07
Sanyam Jain <xerosanyam@gmail.com>
2025-03-25 10:59:45 -0700

    Refactor functions

--- src/robot.ts
+++ src/robot.ts
@@ -1,15 +1,15 @@
+const isBulky = (width: number, height: number, length: number) => {
+	if (width >= 150 || height >= 150 || length >= 150) return true
+	const volume = width * height * length
+	if (volume >= 1_000_000) return true
+	return false
+}
+
+const isHeavy = (mass: number) => {
+	return mass >= 20
+}
+
 export function sort(width: number, height: number, length: number, mass: number) {
-
-	const isBulky = (width: number, height: number, length: number) => {
-		if (width >= 150 || height >= 150 || length >= 150) return true
-		const volume = width * height * length
-		if (volume >= 1_000_000) return true
-		return false
-	}
-	const isHeavy = (mass: number) => {
-		return mass >= 20
-	}
-
 	const itemIsBulky = isBulky(width, height, length)
 	const itemIsHeavy = isHeavy(mass)
 	if (itemIsBulky) {
